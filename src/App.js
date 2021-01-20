f2021fc9e4b0efc53a6132367435cd88904c1620
fmomoreira <felipemoreira.frontend@gmail.com>
2021-01-20 12:08:22 -0300

    upload estrutura menu header e footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React, { Fragment } from 'react'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+
+
+
+import { BrowserRouter } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-     <p>teste</p>
-    </div>
-  );
+   
+        <BrowserRouter>
+          <Fragment>
+              <Header />
+              <Menu />
+               
+              <Footer />
+          </Fragment>
+        </BrowserRouter>
+
+  )
 }
 
-export default App;
+
+export default  App;
